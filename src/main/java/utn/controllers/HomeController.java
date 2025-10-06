44ade9d32c71adf5cb370d422f8f4a8368aae9da
SantyPerezzz <santi.agus.perez.2002@gmail.com>
2025-10-06 12:28:10 -0300

    ARRANCA

--- src/main/java/utn/controllers/HomeController.java
+++ src/main/java/utn/controllers/HomeController.java
@@ -1,18 +1,26 @@
 package utn.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "redirect:/home";
+    public String home(Model model) {
+        model.addAttribute("titulo", "Inicio");
+
+        return "index";
     }
 
-    @GetMapping("/404")
+    @GetMapping("/home")
+    public String home(){
+        return "redirect:/";
+    }
+
+    @GetMapping("/error")
     public String notFound() {
-        return "404";
+        return "error";
     }
 }
