b6d6ec2a46be38762652498ea9d714b2d48c5c0f
SantyPerezzz <santi.agus.perez.2002@gmail.com>
2025-10-14 17:42:34 -0300

    Actualizo pantalla login y agrego LoginController

---
+++ src/main/java/utn/controllers/LoginController.java
@@ -0,0 +1,20 @@
+package utn.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+
+        return "login";
+    }
+
+    @GetMapping("/registro")
+    public String registro(){
+
+        return "registro";
+    }
+}
