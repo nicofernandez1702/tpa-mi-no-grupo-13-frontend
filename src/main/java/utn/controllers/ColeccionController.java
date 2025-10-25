be6febdb76d7463555630eb359b56746ec52f5db
SantyPerezzz <santi.agus.perez.2002@gmail.com>
2025-10-25 13:46:09 -0300

    index.html saco script inutilizado y colecionesController actualizo listarColecciones

--- src/main/java/utn/controllers/ColeccionController.java
+++ src/main/java/utn/controllers/ColeccionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import utn.exceptions.NotFoundException;
 import utn.models.dto.ColeccionDTO;
+import utn.models.dto.HechoDTO;
 import utn.services.ColeccionService;
 import utn.services.MetaMapaApiService;
 
@@ -30,11 +31,18 @@ public class ColeccionController {
         model.addAttribute("titulo", "Colecciones");
         // TODO: agregar los atributos que necesitemos en el model despues de adaptarlo con thymeleaf
 
-        String accessToken = (String) session.getAttribute("accessToken");
+        String accessToken =  (String) session.getAttribute("accessToken");
+        if (accessToken != null) {
+            model.addAttribute("usuario", session.getAttribute("username"));
+        }
+        try {
+            List<ColeccionDTO> colecciones = metaMapaApiService.obtenerColecciones();
 
-        List<ColeccionDTO> colecciones = metaMapaApiService.obtenerColecciones();
-        //List<ColeccionDTO> colecciones = coleccionService.obtenerTodasLasColecciones();
-        model.addAttribute("colecciones", colecciones);
+            model.addAttribute("colecciones", colecciones);
+        }
+        catch (Exception e) {
+            model.addAttribute("No se pudieron cargar las colecciones: ", e.getMessage());
+        }
 
         return "colecciones/colecciones";
     }
