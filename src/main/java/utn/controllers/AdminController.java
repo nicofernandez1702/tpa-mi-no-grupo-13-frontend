578492941da1a74710b3c4448ddfd0fdc2b57587
SantyPerezzz <santi.agus.perez.2002@gmail.com>
2025-11-02 19:09:28 -0300

    Pantallas de admin agregadas y endpoints a las mismas

--- src/main/java/utn/controllers/AdminController.java
+++ src/main/java/utn/controllers/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import utn.models.dto.ColeccionDTO;
+import utn.models.dto.HechoDTO;
 import utn.services.MetaMapaApiService;
 
 import java.util.List;
@@ -29,5 +30,46 @@ public class AdminController {
         return "admin/control_panel";
     }
 
+    @GetMapping("/solicitudes_eliminacion")
+    public String solicitudes(Model model, HttpSession session) {
+
+        model.addAttribute("titulo", "Solicitudes");
+
+        String accessToken =  (String) session.getAttribute("accessToken");
+        if (accessToken != null) {
+            model.addAttribute("usuario", session.getAttribute("username"));
+        }
+
+        return "admin/solicitudes_eliminacion";
+    }
+
+    @GetMapping("/hechos_pendientes")
+    public String hechosPendientes(Model model, HttpSession session) {
+
+        model.addAttribute("titulo", "Hechos Pendientes");
+
+        String accessToken =  (String) session.getAttribute("accessToken");
+        if (accessToken != null) {
+            model.addAttribute("usuario", session.getAttribute("username"));
+        }
+        // TODO funcion obtenerHechosPendientes()
+        // List<HechoDTO> hechosPendientes = metaMapaApiService.obtenerHechosPendientes();
+
+        //model.addAttribute("hechos", hechosPendientes);
+
+        return "admin/hechos_pendientes";
+    }
+
+    @GetMapping("/importar_csv")
+    public String importarCsv(Model model, HttpSession session) {
+        model.addAttribute("titulo", "Importar CSV");
+
+        String accessToken =  (String) session.getAttribute("accessToken");
+        if (accessToken != null) {
+            model.addAttribute("usuario", session.getAttribute("username"));
+        }
+
+        return "admin/importar_csv";
+    }
 
 }
