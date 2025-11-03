473bd4720fabf005e84b78057b5fe2729c29742e
SantyPerezzz <santi.agus.perez.2002@gmail.com>
2025-11-03 19:38:51 -0300

    add: clases SolicitudEliminacion y SolicitudDTO

---
+++ src/main/java/utn/models/dto/SolicitudDTO.java
@@ -0,0 +1,19 @@
+package utn.models.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class SolicitudDTO {
+    private LocalDateTime fechaCreacion;
+    private String estado;
+
+    private String hecho;
+
+
+    private final String motivo;
+
+}
