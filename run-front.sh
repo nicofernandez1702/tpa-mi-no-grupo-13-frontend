4d347eb3d107cc8a1dfc8a6c40a61bdfa2fac998
Nicolas Fernandez <nfernndez@frba.utn.edu.ar>
2025-12-17 21:45:23 -0300

    Script

---
+++ run-front.sh
@@ -0,0 +1,10 @@
+set -e
+
+echo "Compilando front (skip tests)..."
+mvn clean package -DskipTests
+
+echo "Levantando demo..."
+
+nohup java -jar target/demo.jar > demo.log 2>&1 &
+
+echo "Demo levantado en segundo plano"
